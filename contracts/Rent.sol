7f4e4b2690fe10c75f0f9395b2d355cf24ef3668
Marko Mihailovic <marko.mihailovic67@yahoo.com>
2022-08-15 11:26:42 +0200

    Rent seats & unit tests

--- contracts/Rent.sol
+++ contracts/Rent.sol
@@ -1,15 +1,17 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 import "./Token.sol";
+import "./TokenVendor.sol";
+
 interface USDC {
     function transfer(address dst, uint amount) external returns (bool);
     function transferFrom(address src, address dst, uint amount) external returns(bool);
     function balanceOf(address guy) external view returns (uint);
 }
-// balanceOf(account) / totalSupply * ukupanBrMesta
+
 contract Rent {
-    address t;
-    Token token = Token(t);
+    Token token;
+    TokenVendor tokenVendor;
     USDC public usdc;
     uint public amount = 250;
     struct RadnoMesto {
@@ -19,45 +21,40 @@ contract Rent {
         bytes32 hashUlaznice;
     }
     uint n = 100;
-    RadnoMesto[] radnaMesta;
-    constructor(){
+    RadnoMesto[] public radnaMesta;
+    constructor(address tokenAddress, address tokenVendorAddress){
+        token = Token(tokenAddress);
+        tokenVendor = TokenVendor(tokenVendorAddress);
         usdc = USDC(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     }
     function numberOfPlacesForAddress(address _address) public view returns (uint){
         // Koliko neko moze da iznajmi mesta
-        return token.balanceOf(_address) / token.totalSupply() * n;
+        return tokenVendor.stakingBalance(_address) * n / token.totalSupply();
     }
     function numberOfRentedPlacesForAddress(address _address) public view returns (uint) {
         // Koliko je mesta iznajmio
         uint cnt = 0;
-        for(uint i = 0;i < 100; i++) {
-            if(radnaMesta[i].ownerOfSeat == _address && radnaMesta[i].expirationDate > block.timestamp)
+        for(uint i = 0;i < radnaMesta.length; i++) {
+            if(radnaMesta[i].ownerOfSeat == _address && radnaMesta[i].expirationDate >= block.timestamp)
                 cnt++;
         }
         return cnt;
     }
-    function transfer(address payable _to, uint number_of_places, uint numberOfDays) payable public {
-        require(numberOfRentedPlacesForAddress(_to) + number_of_places <= numberOfPlacesForAddress(_to) && number_of_places > 0 , "amount should be > 0"); //provera da li je broj mesta za rezervaciju veci od 0
+    function numberOfFreePlacesForAddress(address _address) public view returns (uint){
+        return numberOfPlacesForAddress(_address) - numberOfRentedPlacesForAddress(_address);
+    }
+    function rentSeat(address payable _to, uint number_of_places, uint numberOfDays) payable public {
+        require(numberOfDays > 0, "Izaberi vise od 0 dana");
+        require(number_of_places > 0, "Izaberi vise od 0 mesta");
+        require(numberOfFreePlacesForAddress(_to) >= number_of_places , "Nije moguce rentirati toliko mesta"); //provera da li je broj mesta za rezervaciju veci od 0
         require(msg.value >= amount * number_of_places , "you dont have enough funds"); //provera da li korisnik ima dovoljno para
         bool sent = usdc.transferFrom(msg.sender, _to, amount * number_of_places * numberOfDays / 30);  //transakcija izmedju korisnika
         require(sent, "Failed to send USDC"); //provera da li je transakcija uspela
         // Registrovati korisnika da je iznajmio
         uint time = block.timestamp + numberOfDays * 1 days;
-        uint cnt = 0;
-        for(uint i = 0;i < n;i++){
-            if(radnaMesta[i].expirationDate < block.timestamp) {
-                radnaMesta[i].ownerOfSeat = _to;
-                radnaMesta[i].renter = msg.sender;
-                radnaMesta[i].expirationDate = time; // dodati broj dana
-                radnaMesta[i].hashUlaznice = hash(_to, cnt,time);
-                cnt++;
-            }
-            if(cnt == number_of_places)
-                break;
+        for(uint i = 0;i < number_of_places;i++){
+            radnaMesta.push(RadnoMesto(_to,msg.sender, time,hash(_to,i,time)));
         }
-    }
-    function numberOfFreePlacesPerUser(address _address) external view returns (uint){
-        return numberOfPlacesForAddress(_address) - numberOfRentedPlacesForAddress(_address);
     }
 
     function hash(address ownerOfSeat, uint counter , uint256 expirationDate) private view returns(bytes32)
@@ -70,6 +67,7 @@ contract Rent {
     {
         bytes32 [] memory h;
         uint256 cnt=0;
+
         for(uint i =0 ; i<n ;i++)
         {
              if(radnaMesta[i].renter==_to && radnaMesta[i].expirationDate > block.timestamp) cnt++;
@@ -77,5 +75,13 @@ contract Rent {
         }
         return h;
     } 
+
+    function ulaz() external view returns(bool) {
+        for(uint i = 0; i < radnaMesta.length;i++) {
+            if(radnaMesta[i].renter == msg.sender && radnaMesta[i].expirationDate >= block.timestamp)
+                return true;
+        }
+        return false;
+    }
 
 }
--- test/Token.js
+++ test/Token.js
@@ -1,35 +1,70 @@
 const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 describe("Token contract", function() {
-    it("Deployment should assign the total supply of tokens to the ownering coins and unstaking coins", async function() {
-        const [owner] = await ethers.getSigners();
-        const Token = await ethers.getContractFactory("Token");
-        const hardhatToken =await  Token.deploy();
-        const ownerBalance = await hardhatToken.balanceOf(owner.address);
-        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    beforeEach(async function() {
+        [owner,wallet1,wallet2] = await ethers.getSigners();
+        Token = await ethers.getContractFactory("Token",wallet1);
+        TokenVendor = await ethers.getContractFactory("TokenVendor",owner);
+        Rent = await ethers.getContractFactory("Rent",wallet1);
+        token = await Token.deploy();
+        tokenVendor = await TokenVendor.deploy(token.address);
+        rent = await Rent.deploy(token.address, tokenVendor.address);
 
-    });
-    it("Should transfer tokens between accounts", async function(){
-        const [owner,addr1,addr2] = await ethers.getSigners();
-        const Token = await ethers.getContractFactory("Token");
+        await token.connect(wallet1).transfer(wallet2.address, 1000);
 
-        const hardhatToken = await Token.deploy();
-        await hardhatToken.transfer(addr1.address,20);
-        expect(await hardhatToken.balanceOf(addr1.address)).to.equal(20);
-    });
-    it("Staking/Unstaking tokens", async function() {
-        const [owner,addr1] = await ethers.getSigners();
-        const Token = await ethers.getContractFactory("Token");
-        const hardhatToken = await Token.deploy();
+        await token.connect(wallet1).approve(tokenVendor.address, 4000);
+        await token.connect(wallet2).approve(tokenVendor.address, 1000);
+
         
-        expect(await
-            hardhatToken.transfer(addr1.address, 50)
-          ).to.changeTokenBalances(hardhatToken, [owner, addr1], [-50, 50]);
-        await hardhatToken.stakeTokens(addr1.address, 10);
-        expect(await hardhatToken.getStakingBalance(addr1.address)).to.equal(10);
-        expect(await hardhatToken.balanceOf(addr1.address)).to.changeTokenBalances(hardhatToken,[addr1],[-10]);
-        await hardhatToken.unstakeTokens(addr1.address,7);
-        expect(await hardhatToken.getStakingBalance(addr1.address)).to.equal(3);
-        expect(await hardhatToken.balanceOf(addr1.address)).to.changeTokenBalances(hardhatToken,[addr1],[7]);
-    });
+    })
+    describe('deployment', function() {
+        it('should mint tokens to wallet1', async function() {
+            expect(await token.balanceOf(wallet1.address)).to.equal(4000);
+        })
+        it('should transfer to wallet2', async function() {
+            expect(await token.balanceOf(wallet2.address)).to.equal(1000);
+        })
+    })
+    describe('stake tokens', function() {
+        it('should stake token', async function() {
+            await tokenVendor.connect(wallet1).stakeTokens(100);
+            await tokenVendor.connect(wallet2).stakeTokens(50);
+
+            expect(await token.balanceOf(wallet1.address)).to.equal(3900);
+            expect(await token.balanceOf(wallet2.address)).to.equal(950);
+            
+            expect(await tokenVendor.stakingBalance(wallet1.address)).to.equal(100);
+            expect(await tokenVendor.stakingBalance(wallet2.address)).to.equal(50);
+        })
+        it('cannot stake more tokens than have', async function() {
+            await expect(tokenVendor.connect(wallet1).stakeTokens(5000)).to.be.revertedWith("Nemas toliko tokena!");
+        })
+    })
+    describe('unstake tokens', function() {
+        it('should unstake token', async function() {
+            await tokenVendor.connect(wallet1).stakeTokens(1000);
+            await tokenVendor.connect(wallet1).unstakeTokens(500);
+            expect(await token.balanceOf(wallet1.address)).to.equal(3500);
+            expect(await tokenVendor.stakingBalance(wallet1.address)).to.equal(500);
+        })
+        it('cannot unstake tokens more than has been staked', async function() {
+            await tokenVendor.connect(wallet1).stakeTokens(1000);
+            await expect(tokenVendor.connect(wallet1).unstakeTokens(1100)).to.be.revertedWith("Nemas toliko stakeovanih tokena!");
+        })
+    })
+    describe('rent place', function() {
+        it('should rent place', async function() {
+            await tokenVendor.connect(wallet1).stakeTokens(100);
+            expect(await rent.numberOfPlacesForAddress(wallet1.address)).to.equal(2);
+            expect(await rent.numberOfFreePlacesForAddress(wallet1.address)).to.equal(2);
+            expect(await rent.numberOfRentedPlacesForAddress(wallet1.address)).to.equal(0);
+            await rent.connect(wallet2).rentSeat(wallet1.address,1,1);
+            expect(await rent.numberOfFreePlacesForAddress(wallet1.address)).to.equal(1);
+            expect(await rent.numberOfRentedPlacesForAddress(wallet1.address)).to.equal(1);
+            await rent.connect(wallet2).rentSeat(wallet1.address,1,1);
+            expect(await rent.numberOfFreePlacesForAddress(wallet1.address)).to.equal(0);
+            expect(await rent.numberOfRentedPlacesForAddress(wallet1.address)).to.equal(2);
+            await expect(rent.connect(wallet2).rentSeat(wallet1.address,1,1)).to.be.revertedWith("Nije moguce rentirati toliko mesta");
+        })
+    })
 });
