b5fc28c5eedcef7dbf20e23af40b3e0e3674e139
Marko Mihailovic <marko.mihailovic67@yahoo.com>
2022-08-15 15:57:01 +0200

    Unrenting seat when unstaking tokens

--- contracts/Rent.sol
+++ contracts/Rent.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 import "./Token.sol";
-import "./TokenVendor.sol";
 
 interface USDC {
     function transfer(address dst, uint amount) external returns (bool);
@@ -11,7 +10,6 @@ interface USDC {
 
 contract Rent {
     Token token;
-    TokenVendor tokenVendor;
     USDC public usdc;
     uint public amount = 250;
     struct RadnoMesto {
@@ -21,15 +19,16 @@ contract Rent {
         bytes32 hashUlaznice;
     }
     uint n = 100;
-    RadnoMesto[] public radnaMesta;
-    constructor(address tokenAddress, address tokenVendorAddress){
+    RadnoMesto[] private radnaMesta;
+
+    constructor(address tokenAddress){
         token = Token(tokenAddress);
-        tokenVendor = TokenVendor(tokenVendorAddress);
         usdc = USDC(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     }
-    function numberOfPlacesForAddress(address _address) public view returns (uint){
+    function numberOfPlacesForAddress(uint numberOfStakedTokens) public view returns (uint){
         // Koliko neko moze da iznajmi mesta
-        return tokenVendor.stakingBalance(_address) * n / token.totalSupply();
+        //return stakingBalance[_address] * n / token.totalSupply();
+        return numberOfStakedTokens * n / token.totalSupply();
     }
     function numberOfRentedPlacesForAddress(address _address) public view returns (uint) {
         // Koliko je mesta iznajmio
@@ -40,13 +39,13 @@ contract Rent {
         }
         return cnt;
     }
-    function numberOfFreePlacesForAddress(address _address) public view returns (uint){
-        return numberOfPlacesForAddress(_address) - numberOfRentedPlacesForAddress(_address);
+    function numberOfFreePlacesForAddress(address _address,uint numberOfStakedTokens) public view returns (uint){
+        return numberOfPlacesForAddress(numberOfStakedTokens) - numberOfRentedPlacesForAddress(_address);
     }
-    function rentSeat(address payable _to, uint number_of_places, uint numberOfDays) payable public {
+    function rentSeat(address payable _to, uint number_of_places, uint numberOfDays,uint numberOfStakedTokens) payable public {
         require(numberOfDays > 0, "Izaberi vise od 0 dana");
         require(number_of_places > 0, "Izaberi vise od 0 mesta");
-        require(numberOfFreePlacesForAddress(_to) >= number_of_places , "Nije moguce rentirati toliko mesta"); //provera da li je broj mesta za rezervaciju veci od 0
+        require(numberOfFreePlacesForAddress(_to,numberOfStakedTokens) >= number_of_places , "Nije moguce rentirati toliko mesta"); //provera da li je broj mesta za rezervaciju veci od 0
         require(msg.value >= amount * number_of_places , "you dont have enough funds"); //provera da li korisnik ima dovoljno para
         bool sent = usdc.transferFrom(msg.sender, _to, amount * number_of_places * numberOfDays / 30);  //transakcija izmedju korisnika
         require(sent, "Failed to send USDC"); //provera da li je transakcija uspela
@@ -65,6 +64,7 @@ contract Rent {
 
     function getUserHash(address _to) public view returns(bytes32[] memory)
     {
+        require(msg.sender == _to);
         bytes32 [] memory h;
         uint256 cnt=0;
 
@@ -82,6 +82,21 @@ contract Rent {
                 return true;
         }
         return false;
+    }
+
+    function getNumberOfPlaces() public view returns(uint){
+        return n;
+    }
+
+    function unrent(uint numberOfPlaces) public {
+            for(uint i = 0;i < radnaMesta.length; i++) {
+                if(radnaMesta[i].ownerOfSeat == msg.sender && block.timestamp <= radnaMesta[i].expirationDate) {
+                    numberOfPlaces--;
+                    radnaMesta[i].expirationDate = 0;
+                }
+                if(numberOfPlaces == 0) break;
+            }
+
     }
 
 }
