a3e009c141e43b90dcfe7b10d02a865a4bd217d5
Zarko <zarkoradenkovic2@gmail.com>
2022-08-15 09:26:21 +0200

    Komit

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,26 +9,4 @@ contract Token is ERC20 {
         owner = msg.sender;
         _mint(msg.sender, 100 * (10 ** 18));
     }
-
-    mapping(address => uint256) stakingBalance;
-    
-    function stakeTokens(address _address, uint256 _amount) public {
-        require(_amount > 0, "Izaberi veci od 0");
-        require(balanceOf(msg.sender) >= _amount, "Nemas toliko tokena!");
-        transferFrom(msg.sender, address(this), _amount);
-        stakingBalance[msg.sender] += _amount;
-    }
-
-    function unstakeTokens(address _address, uint256 _amount) external {
-        require(_amount > 0 && stakingBalance[_address] > _amount);
-
-        stakingBalance[_address] -= _amount;
-
-        approve(owner, _amount);
-        transferFrom(owner, msg.sender ,_amount);
-    }
-
-    function getStakingBalance(address _address) external view returns(uint256){
-        return stakingBalance[_address];
-    }
 }
